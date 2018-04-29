eede47f48ee2d328fa3b32feef418b4ec457eae4
nathan-knight <knathanknight@gmail.com>
2018-04-29 16:12:03 -0400

    Command line arguments control simulation

--- src/codes/knight/aiexperiments/Experiment.java
+++ src/codes/knight/aiexperiments/Experiment.java
@@ -5,11 +5,30 @@ import codes.knight.aiexperiments.collectors.Collectors;
 public class Experiment {
 	
 	public static void main(String args[]) {
-		Collectors collectors = new Collectors()
-				.setShowNearestLine(true)
-				.setNeuronsPerHiddenLayer(5)
-				.enableCenterDropOff()
-				.start();
+		Collectors collectors = new Collectors();
+		for (int i = 0; i < args.length; i++) {
+			switch (args[i].toLowerCase()) {
+				case "-l":
+				case "--layers":
+					collectors.setNumberOfHiddenLayers(Integer.valueOf(args[++i]));
+					break;
+				case "-n":
+				case "--neurons":
+					collectors.setNeuronsPerHiddenLayer(Integer.valueOf(args[++i]));
+					break;
+				case "--draw-nearest-line":
+					collectors.setShowNearestLine(true);
+					break;
+				case "--use-center":
+					collectors.enableCenterDropOff();
+					break;
+				case "--use-nearest-neighbour":
+				case "--use-nearest-neighbor":
+					collectors.enableNeighbourAwareness();
+					break;
+			}
+		}
+		collectors.start();
 	}
 
 }
